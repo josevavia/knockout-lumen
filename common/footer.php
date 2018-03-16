f4d5b97dd5bd992cad05a74cffb4b4581c61c277
Jose Vázquez Viader <jose.vazquez.viader@gmail.com>
2018-03-16 01:55:51 +0100

    Load different apis depending on environmnet

--- common/footer.php
+++ common/footer.php
@@ -1,3 +1,13 @@
+<?php
+function isLocalhost() {
+	$whitelist = array('127.0.0.1', '::1');
+	return in_array($_SERVER['REMOTE_ADDR'], $whitelist);
+}
+?>
 
-<script type="text/javascript" src="http://127.0.0.1:9080/js/v1/api.js"></script>
+<?php if (isLocalhost()) { ?>
+    <script type="text/javascript" src="http://127.0.0.1:9080/js/v1/api.js"></script>
+<?php } else { ?>
+    <script type="text/javascript" src="http://sumbroker.josevavia.es/js/v1/api.js"></script>
+<?php } ?>
 
