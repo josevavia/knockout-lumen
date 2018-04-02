7f0ad0dc37510fb76113b8a7634ac2491db169e7
Jose Vázquez Viader <jose.vazquez.viader@gmail.com>
2018-04-02 12:26:28 +0200

    Update endpoint

--- common/footer.php
+++ common/footer.php
@@ -1,6 +1,6 @@
 <?php if (isLocalhost()) { ?>
     <script type="text/javascript" src="http://127.0.0.1:9080/js/v1/api.js"></script>
 <?php } else { ?>
-    <script type="text/javascript" src="http://sumbrokerapi.josevavia.es/js/v1/api.js"></script>
+    <script type="text/javascript" src="http://api.tuseguromovil.es/js/v1/api.js"></script>
 <?php } ?>
 
