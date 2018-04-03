466e11f64fbe8a1651f660444aeecb351c2a3996
Jose Vázquez Viader <jose.vazquez.viader@gmail.com>
2018-04-03 03:19:04 +0200

    Add policies to distriutors

--- distributors/detail.php
+++ distributors/detail.php
@@ -42,6 +42,42 @@
         </tr>
         </tbody>
     </table>
+
+    <h2>Policies</h2>
+    <table class="table" data-bind="visible: policies.length > 0">
+        <thead>
+        <tr>
+            <td>Id</td>
+            <td>Number</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Product</td>
+            <td>Price</td>
+            <td>Periodicity</td>
+            <td>Status</td>
+            <td>Created at</td>
+            <td>Updated at</td>
+        </tr>
+        </thead>
+        <tbody data-bind="foreach: policies">
+        <tr>
+            <td><span data-bind="text: identifier"></span></td>
+            <td>
+                <a data-bind="attr: {href : '../policies/detail.php?id='+identifier}">
+                    <span data-bind="text: number"></span>
+                </a>
+            </td>
+            <td><span data-bind="text: name"></span></td>
+            <td><span data-bind="text: email"></span></td>
+            <td><span data-bind="text: product.name"></span></td>
+            <td><span data-bind="text: price_total"></span></td>
+            <td><span data-bind="text: periodicity"></span></td>
+            <td><span data-bind="text: status"></span></td>
+            <td><span data-bind="text: created_at"></span></td>
+            <td><span data-bind="text: updated_at"></span></td>
+        </tr>
+        </tbody>
+    </table>
     <a class="btn btn-primary" data-bind="attr: {href : '../stores/create.php?idDistributor='+identifier}">New store</a>
 </div>
 
